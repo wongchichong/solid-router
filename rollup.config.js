bcda03311e7f9c4ed4a5bff194597bc8cc8c3074
wongchichong <wongchichong@gmail.com>
2023-02-10 16:51:57 +0800

    solid sample

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,30 @@
-import babel from "@rollup/plugin-babel";
-import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel"
+import nodeResolve from "@rollup/plugin-node-resolve"
+// import alias from '@rollup/plugin-alias'
 
 export default {
-  input: "src/index.tsx",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "es"
-    }
-  ],
-  external: ["solid-js", "solid-js/web"],
-  plugins: [
-    nodeResolve({
-      extensions: [".js", ".ts", ".tsx"]
-    }),
-    babel({
-      extensions: [".js", ".ts", ".tsx"],
-      babelHelpers: "bundled",
-      presets: ["solid", "@babel/preset-typescript"],
-      exclude: "node_modules/**"
-    })
-  ]
-};
+    input: "src/index.tsx",
+    output: [
+        {
+            file: "dist/index.js",
+            format: "es"
+        }
+    ],
+    // external: ["solid-js", "solid-js/web"],
+    plugins: [
+        nodeResolve({
+            extensions: [".js", ".ts", ".tsx"]
+        }),
+        babel({
+            extensions: [".js", ".ts", ".tsx"],
+            babelHelpers: "bundled",
+            presets: ["solid", "@babel/preset-typescript"],
+            exclude: "node_modules/**"
+        }),
+        // alias({
+        //     entries: [
+        //         { find: 'solid-js', replacement: '../solid-js/packages/solid' },
+        //     ]
+        // })
+    ]
+}
